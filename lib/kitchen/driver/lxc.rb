6c15dd9c9d810614be66631d78d660f377ac4094
Bryan Berry <bryan.berry@gmail.com>
2013-04-22 16:54:56 +0200

    properly use the ipaddress when specified

--- lib/kitchen/driver/lxc.rb
+++ lib/kitchen/driver/lxc.rb
@@ -17,10 +17,13 @@ module Kitchen
 
       def create(state)
         state[:container_id] = instance.name + "-" + ::SecureRandom.hex(3)
-        clone_container(state)
         start_container(state)
-        state[:hostname] = container_ip(state)
-        wait_for_sshd(state[:hostname])
+        if @config[:ipaddress].nil?
+          state[:hostname] = container_ip(state)
+        else
+          state[:hostname] = state[:container_id]
+        end
+        wait_for_sshd(container_ip(state))
       end
 
       def destroy(state)
@@ -29,23 +32,56 @@ module Kitchen
         end
       end
 
+      def login_command(state)
+        combined = config.merge(state)
+        args  = %W{ -o UserKnownHostsFile=/dev/null }
+        args += %W{ -o StrictHostKeyChecking=no }
+        args += %W{ -o LogLevel=#{logger.debug? ? "VERBOSE" : "ERROR"} }
+        args += %W{ -i #{combined[:ssh_key]}} if combined[:ssh_key]
+        args += %W{ -p #{combined[:port]}} if combined[:port]
+        args += %W{ #{combined[:username]}@#{state[:ipaddress]}}
+#        args += %W{ -p #{co}} if @combined[:password]
+        Driver::LoginCommand.new(["ssh", *args])
+      end
+
       protected
 
-      def clone_container(state)
-        run_command("lxc-clone -o #{config[:base_container]} -n #{state[:container_id]}")
+      def build_ssh_args(state)
+        combined = config.merge(state)
+
+        opts = Hash.new
+        opts[:user_known_hosts_file] = "/dev/null"
+        opts[:paranoid] = false
+        opts[:password] = combined[:password] if combined[:password]
+        opts[:port] = combined[:port] if combined[:port]
+        opts[:keys] = Array(combined[:ssh_key]) if combined[:ssh_key]
+
+        [combined[:ipaddress], combined[:username], opts]
       end
 
       def start_container(state)
-        run_command("lxc-start -n #{state[:container_id]} -d")
+        cmd = "lxc-awesome-ephemeral -d -o #{config[:base_container]} -n #{state[:container_id]}"
+        [ :ipaddress, :netmask, :gateway ].each do |opt|
+          unless @config[opt].nil?
+            cmd << " --#{opt} #{@config[opt]} "
+          end
+        end
+        cmd << " 2>&1 > /dev/null"
+        run_command(cmd)
         run_command("lxc-wait -n #{state[:container_id]} -s RUNNING")
       end
 
       def destroy_container(state)
-        run_command("lxc-destroy -n #{state[:container_id]} -f")
+        run_command("lxc-stop -n #{state[:container_id]}")
       end
 
       def container_ip(state)
-        if ::File.exists?(config[:dhcp_lease_file])
+        if !state[:ipaddress].nil?
+          return state[:ipaddress]
+        elsif !@config[:ipaddress].nil?
+          state[:ipaddress] = @config[:ipaddress]
+          return state[:ipaddress]
+        elsif ::File.exists?(config[:dhcp_lease_file])
           30.times do
             leases = ::File.readlines(config[:dhcp_lease_file]).map{ |line| line.split(" ") }
             leases.each do |lease|
