6b1b673a7575d0999fdd80fccd8cc6527a16ba57
Sean Porter <portertech@gmail.com>
2013-05-13 12:13:06 -0700

    added size support, custom base container for tests

--- test/lxc_driver_tests.rb
+++ test/lxc_driver_tests.rb
@@ -7,15 +7,17 @@ require File.join(File.dirname(__FILE__), "..", "lib", "kitchen", "driver", "lxc
 describe Kitchen::Driver::Lxc do
 
   before do
-    @logger_output = StringIO.new
+    @base_container = ENV["BASE_CONTAINER"]
+    raise "You must set BASE_CONTAINER" unless @base_container
     driver_options = {
       :kitchen_root   => File.dirname(__FILE__),
-      :base_container => "ubuntu-1204"
+      :base_container => @base_container
     }
+    @logger_output = StringIO.new
     instance_options = {
       :logger   => Kitchen::Logger.new(:stdout => @logger_output),
       :suite    => Kitchen::Suite.new(:name => "test", :run_list => Array.new),
-      :platform => Kitchen::Platform.new(:name => "ubuntu-1204"),
+      :platform => Kitchen::Platform.new(:name => @base_container),
       :driver   => Kitchen::Driver::Lxc.new(driver_options)
     }
     @instance = Kitchen::Instance.new(instance_options)
@@ -23,9 +25,10 @@ describe Kitchen::Driver::Lxc do
 
   it "can clone a base lxc container" do
     @instance.create
-    @logger_output.string.must_match(/Finished creating <test-ubuntu-1204>/i)
+    container_name = "test-#{@base_container}"
+    @logger_output.string.must_match(/Finished creating <#{container_name}>/i)
     @instance.destroy
-    @logger_output.string.must_match(/Finished destroying <test-ubuntu-1204>/i)
+    @logger_output.string.must_match(/Finished destroying <#{container_name}>/i)
   end
 
 end
