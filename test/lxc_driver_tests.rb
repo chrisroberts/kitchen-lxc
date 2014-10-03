cb53f2884c1bb5c7130f4e373e08b679f6e7f78a
Chris Roberts <code@chrisroberts.org>
2014-10-03 15:41:15 -0700

    Muck with tests. Fix up later.

--- test/lxc_driver_tests.rb
+++ test/lxc_driver_tests.rb
@@ -1,24 +1,25 @@
 require "minitest/spec"
 require "minitest/autorun"
 require "stringio"
+require 'ostruct'
 
 require File.join(File.dirname(__FILE__), "..", "lib", "kitchen", "driver", "lxc")
 
 describe Kitchen::Driver::Lxc do
 
   before do
-    @base_container = ENV["BASE_CONTAINER"]
-    raise "You must set BASE_CONTAINER" unless @base_container
     driver_options = {
       :kitchen_root   => File.dirname(__FILE__),
-      :base_container => @base_container
     }
     @logger_output = StringIO.new
     instance_options = {
       :logger   => Kitchen::Logger.new(:stdout => @logger_output),
       :suite    => Kitchen::Suite.new(:name => "test", :run_list => Array.new),
       :platform => Kitchen::Platform.new(:name => @base_container),
-      :driver   => Kitchen::Driver::Lxc.new(driver_options)
+      :driver   => Kitchen::Driver::Lxc.new(driver_options),
+      :provisioner => Kitchen::Provisioner::Base.new,
+      :busser => Object.new,
+      :state_file => Kitchen::StateFile.new('default', OpenStruct.new(:name => 'ubuntu'))#Kitchen::Busser
     }
     @instance = Kitchen::Instance.new(instance_options)
   end
